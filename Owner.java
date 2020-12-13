d46a0ef5f94361909c503bcb027a5430a548ca54
BartyBoi1128 <58297160+BartyBoi1128@users.noreply.github.com>
2020-12-13 00:32:33 +0000

    Update Owner.java

--- Owner.java
+++ Owner.java
@@ -15,31 +15,40 @@ public class Owner {
         this.ownerid = ownerid;
         this.password = password;
     }
-    
+
+    ////////////Start
     ListView getDisplayList(){
         ListView<String> displayList = new ListView<>();
-       for (int i=0; i<properties.size(); i++){
+        for (int i=0; i<properties.size(); i++){
             displayList.getItems().add(properties.get(i).toString());
         }
         return displayList;
     }
-    
-    ArrayList getOwnerProperties(){
-    return properties;
-    }
-    
-    ListView getDetailedDisplayList(){
-    ListView<String> displayList = new ListView<>();
-    for (int i=0; i<properties.size(); i++){
-            displayList.getItems().add(properties.get(i).toStringDetailed());
+
+    ListView getDiplayListOfPayments(){
+        ListView<String> PaymentsList = new ListView<>();
+        for (int i=0; i<properties.size(); i++){
+            PaymentsList.getItems().add(payments.get(i).toString());
         }
-     return displayList;
+        return PaymentsList;
     }
-    
-    ArrayList getProperties(){
+
+    ArrayList getOwnerProperties(){
         return properties;
     }
 
+    ListView getDetailedDisplayList(){
+        ListView<String> displayList = new ListView<>();
+        for (int i=0; i<properties.size(); i++){
+            displayList.getItems().add(properties.get(i).toStringDetailed());
+        }
+        return displayList;
+    }
+
+    ArrayList getProperties(){
+        return properties;
+    }
+    ///////////End
     /**An accessor method for ownerid */
     String getOwnerId() {
         return ownerid;
@@ -64,11 +73,11 @@ public class Owner {
     String accessEircode(int r) {
         return properties.get(r - 1).getEircode();
     }
-    
+    //////Start
     String accessEircodeReal(int r) {
         return properties.get(r).getEircode();
     }
-
+    ////////End
     /** A mutator for the properties owned by the owner, allows the owner to input a new property */
     void registerNewProperty(String owner, String address, String eircode, double value, String loCat, boolean ppr, int lastPayment) {
         Property newProperty = new Property(owner, address, eircode, value, loCat, ppr, lastPayment);
@@ -101,23 +110,47 @@ public class Owner {
         }
         return false;
     }
-
+    //////////Start
+    boolean makePaymentReal(int b) {
+        if(properties.get(b).getYearOfLastPayment() != LocalDate.now().getYear()) {
+            Payment payment = new Payment(properties.get(b), properties.get(b).getYearOfLastPayment());
+            payment.makePayment();
+            payments.add(payment);
+            properties.get(b).addToPropertyPayments(payment);
+            properties.get(b).setYearOfLastPayment(payment.getYear());
+            return true;
+        }
+        return false;
+    }
+    //////////End
+    
     /** A method that shows how much is to be paid */
     String getPaymentAmount(int b) {
         currentTax = new PropertyTax(properties.get(b - 1));
         currentTax.calcTax();
         return currentTax.toString();
     }
-
+    
+    ////////Start
+    String getPaymentAmountReal(int b) {
+        currentTax = new PropertyTax(properties.get(b));
+        currentTax.calcTax();
+        return currentTax.toString();
+    }
+    ////////End
+    
     /** A mutator that removes a property from an owner */
     void removePropertyFromOwner(int r) {
         properties.remove(r - 1);
     }
     
+    /////////Start
     void removePropertyFromOwnerReal(int r) {
         properties.remove(r);
     }
-
+    ///////////////End
+    
+    
     /** A mutator method which changes the value of whether properties are loaded*/
     void setPropertiesLoaded(boolean a) {
         propertiesLoaded = a;
